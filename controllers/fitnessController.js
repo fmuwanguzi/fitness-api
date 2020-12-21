0e554fa71768a2b9dd370ea315ea1c20ad41f160
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-21 10:28:21 -0500

    model started

--- controllers/fitnessController.js
+++ controllers/fitnessController.js
@@ -8,4 +8,38 @@ router.get('/', (req, res) => {
     })
     .catch((error) => res.send({ error }))})
 
+    router.get('/:id', (req, res) => {
+        models.Fitness.findOne({_id: req.params.id}).then((fitness) => {
+          res.status(200).json({ fitness })
+        })
+        .catch((error) => res.send({ error }))})
+      
+      router.post('/', (req, res) => {
+        models.Fitness.create(req.body).then((fitness) => {
+          res.status(201).json({ fitness })
+        })
+        .catch((error) => res.send({ error }))})
+      
+      router.put('/:id', (req, res) => {
+     
+        
+        const { level } = req.body
+        
+        models.Fitness.update({
+          _id: req.params.id
+        }, {$set: {
+            level
+        }})
+        .then((fitness) => {
+          res.status(201).json({ fitness })
+        })
+        .catch((error) => res.send({ error }))
+      })
+      
+      router.delete('/:id', (req, res) => {
+        models.Fitness.deleteOne({ _id: req.params.id })
+        .then((fitness) => res.status(201).json({ fitness }))
+        .catch((error) => res.send({ error }))
+      })
+
 module.exports = router
--- models/Fitness.js
+++ models/Fitness.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
 
+const planSchema = new mongoose.Schema({
+    day: String,
+    workout: String,
+    reps: number
+})
+
+const nutrition = new mongoose.Schema({
+   placeholder: String 
+})
+
+
 const fitnessSchema = new mongoose.Schema({
     name: String,
-    
     level: {
         type: String,
         required: true 
-    }
-
-
+    },
+    age: {
+        type: Number,
+        min: 1,
+        max: 100
+    },
+    plan: [planSchema],
+    food: [nutrition]
 })
 
 module.exports = mongoose.model('fitness', fitnessSchema)
