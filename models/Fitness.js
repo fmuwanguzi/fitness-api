932bdaed9d3a3e71c253d92d8d91a5f63216d314
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-22 10:03:18 -0500

    [fitnessShema] less feilds

--- models/Fitness.js
+++ models/Fitness.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const planSchema = new mongoose.Schema({
+
     workout: String,
     bodypart: String,
+    level: {
+        type: String,
+        required: true,
+    },
     reps: {
         type: Number,
         required: true
@@ -32,18 +37,10 @@ const nutrition = new mongoose.Schema({
 
 
 const fitnessSchema = new mongoose.Schema({
-    name: String,
-    level: {
-        type: String,
-        required: true 
-    },
-    age: {
-        type: Number,
-        min: 1,
-        max: 100
-    },
-    plan: [planSchema],
-    food: [nutrition]
+
+    plan: [planSchema],//workout 
+    food: [nutrition], //third party api , for food
+    goal: String  // user input 
 })
 
 module.exports = mongoose.model('fitness', fitnessSchema)
