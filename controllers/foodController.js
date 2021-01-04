faa1218e635674ed9c4f2bbcdbd3419ff25dc59a
Felix Muwanguzi <72531389+fmuwanguzi@users.noreply.github.com>
2021-01-04 14:48:54 -0500

    Revert "Web scrapping"

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,17 +1,6 @@
 const router = require('express').Router()
 const Food = require('../models/Food')
 const foodScraper = require('../scrapers/foodScraper')
-const request = require('request')
-const cheerio = require('cheerio')
-
-router.get('/test', async (req,res)=>{
-    request('https://www.eatyourselfskinny.com/cranberry-cocktail-meatballs/',(error,response,html)=>{
-        if (!error && response.statusCode === 200){
-            const $ = cheerio.load(html);
-            const list = $('')
-        }
-    })
-})
 
 // desc get all the foods that are in the database
 // route GET /foods/
@@ -28,7 +17,7 @@ router.get('/', async (req,res)=>{
 
 // desc get back a specific food from db
 // route GET /foods/:name
-router.get('/food:name',async(req,res)=>{
+router.get('/:name',async(req,res)=>{
     try{
         const food = await Food.findOne({name:req.params.name})
         res.status(200).json(food)
@@ -42,54 +31,23 @@ router.get('/food:name',async(req,res)=>{
 // route POST /foods/
 router.post('/', async (req,res)=>{
     try{
-        const steps = []
-        const ingredients = []
-        const facts = []  
-        let image  
-        const url = `https://www.eatyourselfskinny.com/${req.body.food}`
-        request(url, async (error,response,html)=>{
-            if (!error && response.statusCode === 200){
-                const $ = cheerio.load(html)
-                $('.tasty-recipe-ingredients li').each((i,el)=>{
-                    const ingredient = $(el).text()
-                    ingredients.push(ingredient)
-                })
-                $('.tasty-recipe-instructions li').each((i,el)=>{
-                    const step = $(el).text()
-                    steps.push(step)
-                })
-                $('.tasty-recipes-nutrition').each((i,el)=>{
-                    const fact = $(el).text().replace(/\t\t\t\t\t\t\t+/g,'')
-                    facts.push(fact)
-                })
-                $('.tasty-recipes-image img').each((i,el)=>{
-                    image = $(el).attr('src')
-                })
 
-                const food = await Food.findOne({name:req.body.food})
-                if (!food){
-                    const newFood = new Food({
-                        name:req.body.food,
-                        ingredients,
-                        instructions: steps,
-                        nutritionData:facts,
-                        image
-                    })
-                    newFood.save()
-                    res.status(200).json(newFood)
-                } else (
-                    res.status(200).json(food)
-                )
-            }
+        const Url = `${req.body.food}`
+        const foodInfo = foodScraper(Url)
+        const newFood = new Food({
+            name: req.body.name,
+            difficulty : req.body.difficulty
         })
-        // const newFood = new Food({
-        //     name: req.body.food,
-
-        // })
-        // const ingredients = foodInfo.ingredients
-        // const directions = foodInfo.directions
-        // const nutrition = foodInfo.nutrition
-        // console.log(ingredients,directions,nutrition);
+        req.body.ingredients.forEach(ingredient=>{
+            const item = {
+                ingredient: ingredient.ingredient,
+                quantity: ingredient.quantity,
+                unit: ingredient.unit
+            }
+            newFood.ingredients.push(wings)
+        })
+        newFood.save()
+        res.status(200).json(newFood)
     }catch(err){
         console.log(err);
         res.status(400)
