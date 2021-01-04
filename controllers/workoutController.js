60f5ed08134fbafd27577dfd9df82969b5f2c024
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-04 11:28:19 -0500

    [cloudinary and multer] added

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,96 +1,156 @@
-const router = require('express').Router()
-const Workout = require('../models/Workout')
-const workoutScraper = require('../scrapers/workoutScraper')
+const router = require('express').Router();
+const models = require('../models');
+const cloudinary = require('../config/cloudinary');
+const upload = require('../config/multer');
+const Workout = require('../models/Workout');
+require("dotenv").config();
 
-// desc retrieves all workout instances from the db
-// route GET /workouts/
-router.get('/', async (req,res)=>{
+//Post route to send images to cloudinary
+
+router.post('/', upload.single('image'), async (req , res) => {
+    //console.log(cloudinary);
+    console.log(process.env.CLOUD_NAME);
+    console.log(process.env.MONGO_URI);
     try{
-        const workouts = await Workout.find()
-        res.status(200).json(workouts)
-    }catch(err){
-        console.log(err);
-        res.status(400)
+        //upload image to cloudinary
+        const myWorkout = await cloudinary.uploader.upload(req.file.path);
+        //creating a new image which is an object can be whatever your models is 
+        const workout = new Workout({
+            name: req.body.name,
+            picture: myWorkout.secure_url,
+            cloudinary_id: myWorkout.public_id,
+            bodypart: req.body.bodypart,
+            sets: req.body.sets,
+            reps: req.body.reps,
+            description: req.body.description 
+        });
+        await workout.save();
+        res.json(workout);
+    }catch (error){
+        console.log(error)
+    }
+});
+
+//Get route to display all images we have sent to cloudinary 
+
+router.get('/', async (req, res) => {
+    console.log('--get route--')
+    try {
+        const workout = await Workout.find();
+        res.json(workout);
+    }catch(error){
+        console.log(error);
+    }
+});
+
+//get each workout by id this route works
+router.get('/:id', async (req, res) => {
+    try {
+        const workout = await Workout.findById(req.params.id)
+        res.json(workout);
+    }catch(error){
+        console.log(error);
+    }
+});
+
+
+//Get each workout by name
+// router.get('/:name', async (req, res) => {
+//     try {
+//         const workout = await Workout.findOne(req.body.name)
+//         res.json(workout);
+//     }catch(error){
+//         console.log(error);
+//     }
+// });
+
+//Delete route by id this route works
+
+router.delete('/:id' , async (req, res) => {
+    try {
+        //Find workout by the ID
+        const workout = await Workout.findById(req.params.id);
+        //Deleting the workout from cloudinary
+        await cloudinary.uploader.destroy(workout.cloudinary_id);
+        //Delete the workout from the database
+        await workout.remove();
+        res.json(workout);
+    } catch (error){
+        console.log(error);
+    }
+});
+
+//delete workout by name
+// router.delete('/:name' , async (req, res) => {
+//     try {
+//         //Find workout by the ID
+//         const workout = await Workout.findOne(req.body.name)
+//         //Deleting the workout from cloudinary
+//         await cloudinary.uploader.destroy(workout.cloudinary_id);
+//         //Delete the workout from the database
+//         await workout.remove();
+//         res.json(workout);
+//     } catch (error){
+//         console.log(error);
+//     }
+// });
+
+
+//PUT route for changing the workout using id
+router.put('/:id', upload.single('image'), async (req, res ) => {
+    try {
+        let workout = await Workout.findById(req.params.id);
+        //Delete the workout from cloudinary
+        await cloudinary.uploader.destroy(workout.cloudinary_id);
+        //upload new workout to cloudinary
+        const myWorkout = await cloudinary.uploader.upload(req.file.path);
+        const data = {
+            name: req.body.name || workout.name,
+            picture: myWorkout.secure_url || workout.picture,
+            cloudinary_id: myWorkout.public_id || workout.cloudinary_id,
+            bodypart: req.body.bodypart || workout.bodypart,
+            sets: req.body.sets || workout.sets,
+            reps: req.body.reps || workout.reps,
+            description: req.body.description || workout.description  
+        }
+        workout = await Workout.findByIdAndUpdate(req.params.id, data, {
+            new:true
+        });
+        res.json(workout);
+
+    }catch(error){
+        console.log(error)
     }
 })
 
-// desc retrieves one workout instance from the db
-// route GET /workouts/
-router.get('/:name',async(req,res)=>{
-    try{
-        const food = await Food.findOne({name:req.params.name})
-        res.status(200).json(food)
-    }catch(err){
-        console.log(err);
-        res.status(400)
-    }
-})
+//PUT route for changing workout using name
 
-// desc adds a new workout to the deb
-// route POST /workouts/
-router.post('/',(req,res)=>{
-    const {name,bodypart,level,reps,sets,image,description} = req.body
-    try{
-        const newWorkout = new Workout({
-            name,
-            bodypart,
-            level,
-            reps,
-            sets,
-            image,
-            description
-        })
-        newWorkout.save()
-        res.status(200).json(newWorkout)
-    }catch(err){
-        console.log(err);
-        res.status(400)
-    }
-})
+// router.put('/:name', upload.single('image'), async (req, res ) => {
+//     try {
+//         let workout = await Workout.findOne(req.body.name)
+//         //Delete the workout from cloudinary
+//         await cloudinary.uploader.destroy(workout.cloudinary_id);
+//         //upload new workout to cloudinary
+//         const myWorkout = await cloudinary.uploader.upload(req.file.path);
+//         const data = {
+//             name: req.body.name || workout.name,
+//             picture: myWorkout.secure_url || workout.picture,
+//             cloudinary_id: myWorkout.public_id || workout.cloudinary_id,
+                // bodypart: req.body.bodypart || workout.bodypart,
+                // sets: req.body.sets || workout.sets,
+                // reps: req.body.reps || workout.reps,
+                // description: req.body.description || workout.description
+//         }
+//         workout = await Workout.findOneAndUpdate(req.body.name, data, {
+//             new:true
+//         });
+//         res.json(workout);
 
-// desc retrieves all workout instances from the db
-// route PUT /workouts/
-router.put('/edit/:name',async (req,res)=>{
-    console.log(req.params.name);
-    console.log(req.body);
-    const {name,bodypart,level,reps,sets,image,description} = req.body
-    try{
-        //this is copied from foods, but if, else if statements can be made for each param 
-
-        // if (difficulty){
-        //     const workout = await Workout.findOneAndUpdate({
-        //         name: req.params.name
-        //     },{$set : {difficulty:difficulty}}).then(workout=>{
-        //         res.send(workout)
-        //     })
-        // } 
-        // else if(){
-        //     // function needed to take each ingredient test against ingredient in array then $set change
-        // }else if(){
-        //     console.log('');
-        // }else{}
+//     }catch(error){
+//         console.log(error)
+//     }
+// })
 
 
-    }catch(err){
-        console.log(err);
-    }
-})
+module.exports = router; 
 
-// desc retrieves all workout instances from the db
-// route DELETE /workouts/
-router.delete('/delete/:name', async (req,res)=>{
-    try{
-        await Workout.findOneAndDelete({
-            name: req.params.name
-        }).then(workout=>{
-            res.send(workout)
-        })
-
-        // res.status(204).json('completed')
-    }catch(err){
-        console.log(err);
-        res.status(400)
-    }
-})
-
-module.exports = router
--- models/Workout.js
+++ models/Workout.js
@@ -1,34 +1,13 @@
 const mongoose = require('mongoose');
 
 const workoutSchema = new mongoose.Schema({
-
-    name: String,
+    name:String,
+    picture: String,
+    cloudinary_id: String,
     bodypart: String,
-    level: {
-        type: String,
-        required: true,
-    },
-    reps: {
-        type: Number,
-        required: true
-    },
-    sets: {
-        type: Number,
-        required: true
-    },
-    image:{
-        type: String,
-        required: false
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    //goal: String
-    createdAt : { 
-        type : Date, 
-        default: Date.now 
-    }
+    sets: Number,
+    reps: Number,
+    description: String
 })
 
-module.exports = mongoose.model('Workout',workoutSchema)
+module.exports = mongoose.model('workout', workoutSchema);
